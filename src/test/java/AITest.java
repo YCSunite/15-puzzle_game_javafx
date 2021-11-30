3e2d92b9d75fd792cf3fe9d716b7af56e1bf2769
mlaute <mlaute2@uic.edu>
2021-11-30 12:50:01 -0600

    test file added

---
+++ src/test/java/AITest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// handles the 10 test cases for the AI
+
+class AITest {
+
+	public A_IDS_A_15solver ai;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		ai = new A_IDS_A_15solver();
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
