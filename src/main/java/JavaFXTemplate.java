136f599f6cdca99d8b2b00f91f3fa1a3e3d5cedc
Yashasvi Patel <ypatel49@uic.edu>
2021-12-02 23:23:00 -0600

    Transition to game scene added and done

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -1,3 +1,4 @@
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 
 import javafx.fxml.FXMLLoader;
@@ -5,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.nio.file.Paths;
 
 public class JavaFXTemplate extends Application {
 
+	PauseTransition pause = new PauseTransition(Duration.seconds(5));
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
@@ -20,13 +23,22 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+
 			URL url = Paths.get("./src/main/resources/WelcomeScene.fxml").toUri().toURL();
 			Parent root = FXMLLoader.load(url);
+
+			URL url2 = Paths.get("./src/main/resources/GameScene.fxml").toUri().toURL();
+			Parent root2 = FXMLLoader.load(url2);
 
 			primaryStage.setTitle("Welcome to 15 puzzle!!!");
 			Scene s1 = new Scene(root, 700,500);
 			s1.getStylesheets().add("WelcomeScene.css");
+			Scene s2 = new Scene(root2, 700,500);
+			s2.getStylesheets().add("GameScene.css");
+		    pause.setOnFinished(event->primaryStage.setScene(s2));
+			pause.play();
 			primaryStage.setScene(s1);
+
 			primaryStage.show();
 
 
