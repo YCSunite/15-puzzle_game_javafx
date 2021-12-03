e931fb9fddf169f306a2e35e1b361927d2c707f5
Yashasvi Patel <ypatel49@uic.edu>
2021-12-02 22:13:27 -0600

    Added controller classes

---
+++ src/main/java/GameModel.java
@@ -0,0 +1,12 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+//controller for the game scene
+public class GameModel implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/main/java/transitionScreen.java
@@ -0,0 +1,12 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+//controller for welcome to game scene
+public class transitionScreen implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
