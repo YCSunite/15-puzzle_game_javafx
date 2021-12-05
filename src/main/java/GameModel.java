751a3940bf00580d548bdc601779acc3d02662f5
mlaute2 <mlaute2@uic.edu>
2021-12-04 21:21:26 -0600

    drawGameBoard() completed

--- src/main/java/GameModel.java
+++ src/main/java/GameModel.java
@@ -65,23 +65,35 @@ public class GameModel implements Initializable {
 	
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+    	
     }
     
     /* this is the main function for all of the tiles in the gameboard */
-    EventHandler<ActionEvent> gameButtonHandler = new EventHandler<ActionEvent>() {
-    		public void handle(ActionEvent e) {
+    public void makeMove() {
     			// TODO: check that it's a valid move
-    			
-    			
-    			
+    			// TODO: swap the tiles
+    			drawGameBoard(); 			
     			// TODO: check for a win
-    		}
-    };
+    }
     
     /* updates the gameBoard buttons with the values of the internal int array's values */
     private void drawGameBoard() {
-
+    	this.gameButton0.setText(this.gameBoardData.get(0).toString());
+    	this.gameButton1.setText(this.gameBoardData.get(1).toString());
+    	this.gameButton2.setText(this.gameBoardData.get(2).toString());
+    	this.gameButton3.setText(this.gameBoardData.get(3).toString());
+    	this.gameButton4.setText(this.gameBoardData.get(4).toString());
+    	this.gameButton5.setText(this.gameBoardData.get(5).toString());
+    	this.gameButton6.setText(this.gameBoardData.get(6).toString());
+    	this.gameButton7.setText(this.gameBoardData.get(7).toString());
+    	this.gameButton8.setText(this.gameBoardData.get(8).toString());
+    	this.gameButton9.setText(this.gameBoardData.get(9).toString());
+    	this.gameButton10.setText(this.gameBoardData.get(10).toString());
+    	this.gameButton11.setText(this.gameBoardData.get(11).toString());
+    	this.gameButton12.setText(this.gameBoardData.get(12).toString());
+    	this.gameButton13.setText(this.gameBoardData.get(13).toString());
+    	this.gameButton14.setText(this.gameBoardData.get(14).toString());
+    	this.gameButton15.setText(this.gameBoardData.get(15).toString());
     }
 
     public void exitGame(){
